bac8ea77226efb7433bf534e4787f4082973a4ab
Carlos Andres Pacheco Fuentes <carloandres4020@gmail.com>
2022-12-27 21:31:02 -0500

    First commit

---
+++ resources/views/livewire/nav.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false" v-pre>
+            Configuraciones
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="">Tipos de vehiculos</a>
+            <!-- {{ route('logout') }} -->
+        </div>
+    </li>
+</div>
